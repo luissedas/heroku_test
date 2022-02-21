72d39a5558a4e9ba82a889b366710619fce96ad2
luissedas <luis.sedas1@gmail.com>
2022-02-20 21:23:52 -0600

    pylint pass

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-import os
+"""Added Docstrings"""
 import math
 
-def func1(a, b) -> str:
-    return math.floor(a + b)
+def func1(param_a, param_b) -> str:
+    """_summary_
 
-def func2(a, b, c) -> int:
-    return os.getcwd()
+    Args:
+        param_a (_type_): _description_
+        param_b (_type_): _description_
 
-def func3():
-    return "Hello World"
+    Returns:
+        str: _description_
+    """
+    return math.floor(param_a + param_b)
+
+def func2(param_a, param_b, param_c) -> int:
+    """_summary_
+
+    Args:
+        param_a (_type_): _description_
+        param_b (_type_): _description_
+        param_c (_type_): _description_
+
+    Returns:
+        int: _description_
+    """
+    return math.floor(param_a + param_b + param_c)
+
+def func3(name):
+    """_summary_
+
+    Args:
+        name (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return f"Hello {name}"
