24551679b27b2ecd204196eba628e501d15e6dbb
luissedas <luis.sedas1@gmail.com>
2022-02-20 23:31:13 -0600

    changes

--- main.py
+++ main.py
@@ -2,37 +2,13 @@
 import math
 
 def func1(param_a, param_b) -> str:
-    """_summary_
-
-    Args:
-        param_a (_type_): _description_
-        param_b (_type_): _description_
-
-    Returns:
-        str: _description_
-    """
+    """docstring"""
     return math.floor(param_a + param_b)
 
 def func2(param_a, param_b, param_c) -> int:
-    """_summary_
-
-    Args:
-        param_a (_type_): _description_
-        param_b (_type_): _description_
-        param_c (_type_): _description_
-
-    Returns:
-        int: _description_
-    """
+    """docstring"""
     return math.floor(param_a + param_b + param_c)
 
 def func3(name):
-    """_summary_
-
-    Args:
-        name (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
+    """docstring"""
     return f"Hello {name}"
