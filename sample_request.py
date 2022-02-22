cda195983058fe752926546ff3032041480f4952
luissedas <luis.sedas1@gmail.com>
2022-02-21 23:49:03 -0600

    added live_post

--- sample_request.py
+++ sample_request.py
@@ -3,18 +3,20 @@ import json
 import requests
 
 data = {
-        "age": 38,
-        "workclass": "Private",
-        "education": "Doctorate",
-        "marital-status": "Married-civ-spouse",
-        "occupation": "Exec-managerial",
-        "relationship": "Husband",
-        "race": "White",
-        "sex": "Male",
-        "hours-per-week": 60,
-        "native-country": "United-States"
-        }
+    "age": 38,
+    "workclass": "Private",
+    "education": "Doctorate",
+    "marital-status": "Married-civ-spouse",
+    "occupation": "Exec-managerial",
+    "relationship": "Husband",
+    "race": "White",
+    "sex": "Male",
+    "hours-per-week": 60,
+    "native-country": "United-States"
+}
 
-r = requests.post("http://127.0.0.1:8000/", data=json.dumps(data))
+r = requests.post("https://luis-heroku-app.herokuapp.com/",
+                  json=data)
 
+print(r.status_code)
 print(r.json())
